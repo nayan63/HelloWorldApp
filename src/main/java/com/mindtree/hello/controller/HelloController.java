b7463e32c2b9ba9151ab32f6b52d04b2b56fcfa6
nayan <nayanhatai63@gmail.com>
2022-08-09 13:11:48 +0530

    All file are added with a commit

---
+++ src/main/java/com/mindtree/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mindtree.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("display/{name}")
+	public String display(@PathVariable String name)
+	{
+		return "Hello "+name;
+	}
+}
