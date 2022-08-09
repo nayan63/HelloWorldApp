51788d0bd20813aa300c46c2e4e454374dcef60a
nayan63 <nayanhatai63@gmail.com>
2022-08-09 21:01:03 +0530

    Added Hello function to print Hello

--- src/main/java/com/mindtree/hello/controller/HelloController.java
+++ src/main/java/com/mindtree/hello/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.mindtree.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,11 @@ public class HelloController {
 	public String display2(@PathVariable Long number)
 	{
 		return "Hello "+number;
+	}
+	
+	@PostMapping("/Hello")
+	public String Hello()
+	{
+		return "Hello";
 	}
 }
